cc410edda0e2b0d6d4848cc81bed44a683bf3feb
SanjayDookhoo <sanjaydookhoo@msn.com>
2019-08-31 04:06:05 -0400

    Created first components (list and card)

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import List from "./List";
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>hello</h2>
+      <List title="first list"/>
+    </div>
+  );
+}
+
+export default App;
